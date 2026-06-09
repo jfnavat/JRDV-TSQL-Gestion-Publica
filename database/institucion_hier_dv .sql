cb84fb48b62a5c31572cf1e45047c6e52403b928
jfnavat <jfnavat@gmail.com>
2026-06-09 18:12:06 -0500

    Update institucion_hier_dv .sql

--- database/institucion_hier_dv .sql
+++ database/institucion_hier_dv .sql
@@ -1,1 +1,20 @@
-
+-- JRDV para Institución
+CREATE OR REPLACE JSON RELATIONAL DUALITY VIEW institucion_hier_dv AS
+SELECT JSON {
+    'id_institucion'	: i.id,
+    'nombre'		: i.nombre,
+    'codigo'		: i.codigo,
+    'tipo'			: i.tipo,
+    -- Objeto padre para reflejar la jerarquía:
+    'pertenece_a'    : (
+        SELECT JSON {
+            'id'		: p.id,
+            'nombre'	: p.nombre,
+            'codigo'	: p.codigo,
+            'tipo'	: p.tipo
+        }
+        FROM institucion p 
+        WHERE p.id = i.parent_id
+    )
+}
+FROM institucion i;
